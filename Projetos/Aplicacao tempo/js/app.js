786b926c5bf381561871de8417fad9db51260b2d
Pablo Matos Lima <pablomtlima1@gmail.com>
2023-03-13 14:04:26 -0300

    place holder

--- Projetos/Aplicacao tempo/js/app.js
+++ Projetos/Aplicacao tempo/js/app.js
@@ -1,38 +1,49 @@
 const cityForm = document.querySelector('[data-js="change-location"]')
 const citynameContainer = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
-const cityTemperatureContainer = document
-    .querySelector('[data-js="city-temperature"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
 const cityCard = document.querySelector('[data-js="city-card"]')
-let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+let timeImg = document.querySelector('[data-js="time"]')
 
-cityForm.addEventListener('submit', async event => {
-    event.preventDefault()
-    
-    const inputValue = event.target.city.value
-    const [ { Key , LocalizedName }] = await getCityData(inputValue)
-    const [ { WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key)
-    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-    
+const showCityCard = () => {
+
     if( cityCard.classList.contains('d-none') ){
         cityCard.classList.remove('d-none')
     }
+}
 
-    if(IsDayTime){
-        timeImg.src = "./src/day.svg"
-    }else{
-        timeImg.src = "./src/night.svg"
-    }
-    
+const showCItyWetherInfo = async cityName=> {
+
+    const [ { Key , LocalizedName }] = await getCityData(cityName)
+    const [ { WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key)
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+    timeImg.src = IsDayTime ? "./src/day.svg" : "./src/night.svg"   
     timeIconContainer.innerHTML = timeIcon
     citynameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+
+if (localStorage.getItem('cityName')){
+    showCityCard()
+    showCItyWetherInfo(localStorage.getItem('cityName'))
+}
+
+cityForm.addEventListener('submit', event => {
+    event.preventDefault()
     
+    const inputValue = event.target.city.value
+    
+    
+    showCityCard()
+    showCItyWetherInfo(inputValue)
 
-    console.log( Key, LocalizedName, WeatherText , Temperature.Metric.Value )
-
+    
+    localStorage.setItem('cityName', inputValue)
     cityForm.reset()
 
 })
+
